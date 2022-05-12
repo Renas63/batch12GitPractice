c6570645d1a0c438184ea3275a9e698901604f0b
[lucian1333] <[mlmlucian@gmail.com]>
2022-05-12 18:53:15 -0500

    3rd change

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
+        System.out.println("This is batch12 3rd practice");
     }
 }
