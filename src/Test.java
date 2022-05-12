7a375d35faa1a542b5c026ca024b0cc6815f26c8
renas63 <renasaktas63@gmail.com>
2022-05-12 15:46:44 -0500

    2nd push

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
+        System.out.println("This is batch12 2nd practice");
     }
 }
