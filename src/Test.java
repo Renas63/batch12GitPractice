1bc176ed31fd60e50ac446781b616cd2a6a2853d
Elif O. Kaynak <kaynakef@gmail.com>
2022-05-12 19:59:04 -0400

    Updated with uppercase

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
-        System.out.println("something from elif");
+        System.out.println("Something from elif");
     }
 }
