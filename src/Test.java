1835772ceefb757d99ae58a8c31a3ff7acc7470e
Anatolie Molosag <anatolie.molosag94@gmail.com>
2022-05-12 20:11:06 -0500

    Hello

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,6 @@ public class Test {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
         System.out.println("3rd practice");
-
-
-        System.out.println("Toni");
-
+        System.out.println("Toni ");
     }
 }
