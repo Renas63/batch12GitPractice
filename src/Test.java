9ae17ab3b76aa21ac31ae066aa2d66cbe2d7f8a1
elena <elenapinar87@gmail.com>
2022-05-12 20:33:23 -0500

    n fgntu,yjh

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
+        System.out.println("Elena");
+        System.out.println("I can do this");
     }
 }
