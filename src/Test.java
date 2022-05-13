f1695e88083c370d61e31c7cdb67e854b5e75795
Andrii <shmen91@gmail.com>
2022-05-12 20:13:18 -0500

    from Andrew

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
-        System.out.println("3rd practice");
+
+
+        String name = "Andrii";
+        System.out.println(name);
     }
 }
