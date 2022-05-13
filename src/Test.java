9c2194c3b7f2e3bb717267d2ae8731f6253806db
Nuradil Zhanadil uulu <nuradilzhanadiluulu@Nuradils-MacBook-Pro.local>
2022-05-12 20:17:50 -0500

    pushing my first code

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,16 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
-
-        System.out.println("Something from elif");
         System.out.println("3rd practice");
+        System.out.println("Nuradil");
+        System.out.println("task for 7 min");
+        System.out.println("another change for pushing the code");
 
-        System.out.println("4th practice");
+
+
+
+
+
 
     }
 }
