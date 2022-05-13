cdd4d7c97eac150d78d4f8a7a5aa6b98497a630d
Anatolie Molosag <anatolie.molosag94@gmail.com>
2022-05-13 16:54:58 -0500

    Approval

--- src/Test.java
+++ src/Test.java
@@ -14,13 +14,12 @@ public class Test {
         System.out.println("packers");
         System.out.println("");
        
-
         System.out.println("toni practice");
-        String str = "Successfully completed java and Started Selenium";
-        System.out.println("");
+        String str = " Successfully completed java and Started Selenium ";
+        System.out.println(" ");
        
-        System.out.println("Automation");
-        //missing something //
+        System.out.println(" Automation ");
+        //missing something//
      
 
 
