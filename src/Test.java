25da2daeb55e9a8d83717f7ba403f533ecf3c2cc
angelore <valeriaore17@.com>
2022-05-12 19:13:01 -0500

    added my name in code block

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
-        System.out.println("3rd practice");
+        System.out.println("Angel was here");
+        System.out.println("Valeria Ore");
     }
 }
