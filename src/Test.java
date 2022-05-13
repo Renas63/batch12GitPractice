cf3d4c6d56d59ef1932928cf1a761d701ef56804
Sedo <99925937+sedenodabas@users.noreply.github.com>
2022-05-12 20:25:23 -0500

    Seden first try

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,13 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
+        System.out.println("3rd practice");
+        System.out.println("Ainur");
+        System.out.println("tHIS is an updated code");
+        System.out.println("This is for push");
+
     }
 }
