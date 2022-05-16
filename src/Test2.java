c823f894ae9453cca3ea7b5e52dbeb0caaaf2b63
HUSSEIN AlGAILANI <mastang.gtr.2009.hf@gmail.com>
2022-05-15 20:55:54 -0500

    It's me again and it is sunday

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,7 @@
 public class Test2 {
     public static void main(String[] args) {
+
         System.out.println("Here is another file");
+        System.out.println("It's sunday and I' happy");
     }
 }
