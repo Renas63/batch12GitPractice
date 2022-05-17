a4043aa494ba3a22b9f4a43f1946d42372e8df89
MarjanaKadyr <mvrjvn@gmail.com>
2022-05-16 19:30:28 -0700

    added a comment

--- src/Test.java
+++ src/Test.java
@@ -33,8 +33,8 @@ public class Test {
         System.out.println("Please work on your branch first and push it to main later.");
 
         System.out.println("First try");
+        System.out.println("Hey! Marjana");
 
-        System.out.println("Hey! Marjana is here!");
 
 
 
