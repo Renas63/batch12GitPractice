42d694ae03c18a5087fc93f4008dcfbd47b5a5df
Narmandakhb <Narmandakh.bayaraa@gmail.com>
2022-05-19 00:55:40 -0500

    add

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is batch12 practice");
         System.out.println("This is batch12 2nd practice");
+System.out.println("first test");
         System.out.println("first test");
 
     }
